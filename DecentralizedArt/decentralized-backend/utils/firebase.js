2410bf8a778fb42fdfa89b3bd9bf5cf6a96313b0
SIBOMANA GLORRY <glorrysibomana758@gmail.com>
2024-03-04 19:46:16 +0300

    Update firebase.js

--- DecentralizedArt/decentralized-backend/utils/firebase.js
+++ DecentralizedArt/decentralized-backend/utils/firebase.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../service-account-key.json')
+
 
 admin.initializeApp({
     credential: admin.credential.cert({
